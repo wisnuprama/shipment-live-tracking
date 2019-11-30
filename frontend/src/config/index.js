66bfec5749b8fa63aea6c3ff18fc3e334d62a6ae
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-12-01 01:15:35 +0700

    fix: finalize migration to ssl

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -3,4 +3,4 @@ export const GOOGLE_MAPS_API_KEY = "AIzaSyBThRAG3ComsOAhNqLNyzegm7ZrLVz9FtU";
 export const IS_DEVELOPMENT = process.env.NODE_ENV === "development";
 export const BACKEND_URL = IS_DEVELOPMENT
   ? "http://localhost:5000"
-  : "http://13.76.129.174:5000";
+  : "https://pdb.wisnuprama.tech/livetracking";
