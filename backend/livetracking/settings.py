3c0fe2f786ed4cdabc38c0a8d72ef75ca32cd5b5
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-12-01 19:49:09 +0700

    fix: update settings so backend can connect to 2 nodes

--- backend/livetracking/settings.py
+++ backend/livetracking/settings.py
@@ -11,18 +11,31 @@ IS_DEVELOPMENT = os.getenv('FLASK_ENV', 'development') == 'development'
 HOST = '0.0.0.0'
 
 # datasource
-DATABASE = {
-    'keyspace': {
-        'name': 'livetracking',
-        'replication_factor': 1 if IS_DEVELOPMENT else 2,
-    },
-    'config': {
-        'default_keyspace': 'livetracking',
-        'hosts': ['172.21.128.3', ],
-        'retry_connect': True,
-        'port': 9042
+if IS_DEVELOPMENT:
+    DATABASE = {
+        'keyspace': {
+            'name': 'livetracking',
+            'replication_factor': 1,
+        },
+        'config': {
+            'default_keyspace': 'livetracking',
+            'hosts': ['172.21.128.3', ],
+            'retry_connect': True,
+            'port': 9042
+        }
     }
-}
-
+else:
+    DATABASE = {
+        'keyspace': {
+            'name': 'livetracking',
+            'replication_factor': 2,
+        },
+        'config': {
+            'default_keyspace': 'livetracking',
+            'hosts': ['172.21.128.3', '172.21.128.4'],
+            'retry_connect': True,
+            'port': 9042
+        }
+    }
 
 TEMPLATES_DIR = os.path.join(os.path.join(BASE_DIR, 'web'), 'templates')
