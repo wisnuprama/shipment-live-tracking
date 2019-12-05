2504152859f5253337d3c2bfea9b7a037d9afc25
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-12-05 12:20:55 +0700

    fix: bug and performance, bulk create

--- frontend/src/pages/ShipmentPage/CoordTracker.jsx
+++ frontend/src/pages/ShipmentPage/CoordTracker.jsx
@@ -92,7 +92,8 @@ export default function CoordTracker({
     latestCoord,
     setLatestCoord,
     destinationLat,
-    destinationLng
+    destinationLng,
+    bulkCoords
   ]);
 
   React.useEffect(() => {
